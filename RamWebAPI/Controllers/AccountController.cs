8bae419d4cffd63adbe44b79db110f6491fab99f
Евгений Егоров <evstroyka@gmail.com>
2023-12-22 12:46:01 +0300

    Добавьте файлы проекта.

---
+++ RamWebAPI/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RamWebAPI.Models;
+using System.Threading.Tasks;
+
+namespace RamWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : Controller
+    {
+        private readonly DatabaseContext context;
+        public AccountController(DatabaseContext context)
+        {
+            this.context = context;
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] User model)
+        {
+            //Проверка валидации и обработки ошибок
+            var newUser = new User
+            {
+                UserName = model.UserName,
+                Password = model.Password,
+            };
+            context.Users.Add(newUser);
+            await context.SaveChangesAsync();
+            return Ok(new { Message = "Пользователь успешно зарегестрирован." });
+        }
+    }
+}
---
+++ RamWebAPI/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RamWebAPI.Models;
+using System.Collections.Generic;
+
+namespace RamWebAPI
+{
+    public class DatabaseContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+            //Создаем базу данных при первом вызове
+            Database.EnsureCreated();
+        }
+    }
+}
